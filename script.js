48d3317f4425c9b28ad320095a071aa44503142d
Chritin <32113749+Chritin@users.noreply.github.com>
2018-12-05 14:57:45 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#button2").click(function() {
+    $("#hey").fadeToggle();
+});
+$("#button1").click(function() {
+    $("#heyy").slideToggle();
+});
+$("#button3").click(function() {
+    $("#heyyy").toggle();
+});
